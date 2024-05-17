76364b2058e76a510b1f6fe2b5b53378dba686e1
Fezz <jonnicwolf@gmail.com>
2024-05-17 17:28:20 -0400

    "Updated Section_1 component: replaced h1 tag with H1 component, removed empty p tag, and adjusted padding and font size styles."

--- present_scents/src/Components/Home/Section_1.jsx
+++ present_scents/src/Components/Home/Section_1.jsx
@@ -6,7 +6,7 @@ const Section_1 = () => {
     <Container>
       <Box_Img />
       <Box_Text >
-        <h1>Our Brand Story</h1>
+        <H1>Our Brand Story</H1>
         <div>
           <p>Welcome to Present Scents, where we're all about flipping the script on self-criticism and embracing self-empowerment!</p>
           <p>Picture this: you know those moments when you slip up and suddenly you're in full-on self-critique mode? Yeah, we've been there. But here's the deal  we're all about changing that narrative.</p>
@@ -14,7 +14,6 @@ const Section_1 = () => {
           <p>Cue the positive affirmations! We're talking about mantras like "I am worthy", "I am enough", "I am strong".</p>
           <p>Sure, it feels a bit weird at first, like we're talking to ourselves in a foreign language. But guess what? These silly phrases brought life and power, to all of use here Present Scents.</p>
           <p>So, if you're tired of the negativity and ready to embrace some self-love, you're in the right place. Shop with us, and let's rewrite that inner dialogue together. Because seriously, you deserve all the love and kindness in the world, flaws and all.</p>
-          <p></p>
         </div>
       </Box_Text>
       <Box_Text />
@@ -38,10 +37,14 @@ const Box = styled.div`
 const Box_Text = styled(Box)`
   display: flex;
   flex-direction: column;
-  padding: 15px;
+  padding: 25px;
 `;
 const Box_Img = styled(Box)`
 `
+const H1 = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+`;
 
 
 export default Section_1;
